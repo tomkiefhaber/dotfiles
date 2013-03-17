c1f67b62f7ab21fed95b24b5e909dcb8ed5923af
Jim Garvin <jim@thegarvin.com>
2013-03-17 19:35:16 -0400

    Fixed get.vimfiles.sh

--- get.vimfiles.sh
+++ get.vimfiles.sh
@@ -9,12 +9,15 @@
 
 # Put your github username on the next line
 REPO_OWNER="coderifous"
+
 # REPO_HOST will generally be "github.com", but may be changed to something else
 # if you're using a different git hosting service or if you have configured
 # an alias in ~/.ssh/config, e.g. because you're using multiple identities.
 REPO_HOST="github.com"
+
 # The next line should contain the name of the repository.
 REPO_NAME="dotfiles"
+
 GIT_REPO_URL="git@$REPO_HOST:$REPO_OWNER/$REPO_NAME.git"
 
 echo -e "\033[32mDownloading repository."
@@ -23,21 +26,18 @@ echo -e "\033[0m"
 git clone $GIT_REPO_URL
 
 echo
-echo -e "\033[32mUpdating submodules."
-echo -e "\033[0m"
-
-cd $REPO_NAME
-git submodule update --init
-
-echo
 echo -e "\033[32mCreating dotfile links in home dir."
 echo -e "\033[0m"
 
-VIMHOME=`pwd`"/vim"
+ln -snf `pwd`/$REPO_NAME/vim ~/.vim
+ln -snf ~/.vim/vimrc   ~/.vimrc
+ln -snf ~/.vim/gvimrc  ~/.gvimrc
 
-ln -s $VIMHOME      ~/.vim
-ln -s ~/.vim/vimrc  ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+echo
+echo -e "\033[32mInstalling plugins."
+echo -e "\033[0m"
+
+$REPO_NAME/vim/plugins install
 
 echo
 echo -e "\033[32mCreating ~/.vim_tmp: where vim is configured to store temporary files."
